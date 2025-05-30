bb94b302ef1147c3e4e7c49768c001b2162fc363
GOLD SOLAR s.r.l <info@goldsolarweb.com>
2025-05-31 01:09:44 +0200

    sync remote with local changes

--- models/project_sprint.py
+++ models/project_sprint.py
@@ -1,11 +1,9 @@
+# project_scrum/models/project_sprint.py
 from odoo import models, fields
 
 class ProjectSprint(models.Model):
     _name = 'project.sprint'
     _description = 'Project Sprint'
-
     name = fields.Char(string='Nome Sprint', required=True)
     date_start = fields.Date(string='Data Inizio')
     date_end = fields.Date(string='Data Fine')
-    project_id = fields.Many2one('project.project', string='Progetto', required=True)
-    sequence = fields.Integer(string='Sequenza', default=10)
